88c1c982345d915525a0d3a45d38c1a85dc4cc73
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-03-26 00:18:24 +0600

    added header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h2>Confused Buyer</h2>
-            <h4>Choose Bikes</h4>
+        <div className='header'>
+            <h3 className='header-title'>Confused Buyer</h3>
+            <h4>Choose 4 Bikes</h4>
         </div>
     );
 };
