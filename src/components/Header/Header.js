34d461ba082bc3beef423d289e9ac1644651a28f
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-03-25 23:57:49 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Confused Buyer</h2>
+            <h4>Choose Bikes</h4>
+        </div>
+    );
+};
+
+export default Header;
